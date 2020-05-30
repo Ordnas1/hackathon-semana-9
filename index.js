99917d17e572f495f442846e06e4f1fb275394bc
Ordnas1 <sandromarcelo.peirano@gmail.com>
2020-05-30 11:27:19 -0500

    Reto 2

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Reto 1
+
 const caesarShift = function(char, shift, low, high) {
     let charCode = char.charCodeAt();
     if (shift >= 0) {
@@ -37,3 +39,55 @@ const caesarencode = function(cypherNumber, cypherText) {
 const caesardecode = function(cypherNumber,textDecode) {
     return caesarencode(-cypherNumber, textDecode)
 }
+
+// Reto 2
+
+//Vars 
+
+
+//Funciones
+function tirarDado() {
+    return Math.floor(Math.random() * 6) + 1
+}
+
+function play() {
+    let apuestaRonda, dadoJugador, dadoCPU;
+    let dinero = 5;
+    while (dinero > 0 && dinero < 200) {
+        alert(`Usted tiene ${dinero} soles`)
+
+        do {
+            apuestaRonda = Number(prompt("Favor de indicar cuanto va a apostar"))
+
+            if (apuestaRonda > dinero || apuestaRonda == 0) {
+                alert("Cantidad no válida")
+            }
+        } while (apuestaRonda > dinero || apuestaRonda == 0)
+
+        do {
+            dadoJugador = Number(prompt("Ingrese el número que quiere apostar"))
+            
+            if(dadoJugador < 1 || dadoJugador > 6) {
+                alert("Apuesta no valida")
+            }
+        } while (dadoJugador < 1 || dadoJugador > 6) 
+        dadoCPU = tirarDado()
+        alert(`Resultado
+        Usted ha tirado ${dadoJugador}
+        El CPU ha tirado ${dadoCPU}`)
+
+        if (dadoJugador == dadoCPU) {
+            alert(`Ha ganado. Recibe ${apuestaRonda * 2} Soles`)
+            dinero += apuestaRonda
+        } else {
+            alert(`Ha perdido ${apuestaRonda} soles`)
+            dinero -= apuestaRonda
+
+        }
+
+        if (dinero == 0) {
+            alert("Ha perdido. Mejor suerte la próxima")
+        }
+    }
+}
+    
